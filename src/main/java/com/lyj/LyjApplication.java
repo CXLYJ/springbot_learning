0044ce126ed3490ef001627d68235f9e334c538b
‘cxlyj <1421805850@qq.com>
2020-01-13 22:56:47 +0800

    错误配置

--- src/main/java/com/lyj/LyjApplication.java
+++ src/main/java/com/lyj/LyjApplication.java
@@ -3,15 +3,18 @@ package com.lyj;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @MapperScan("com.lyj.dao")
 //定时器启动
 @EnableScheduling
 @EnableAsync //开启异步任务
+@ComponentScan(basePackages = {"com.lyj.repository"})
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class LyjApplication {
 
 	public static void main(String[] args) {
